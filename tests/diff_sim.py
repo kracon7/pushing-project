232443bcca3a60537f30d636d6726bb4876faa7a
Jiacheng <ethancooper007@gmail.com>
2022-12-16 14:19:52 -0600

    Diff sim loss averaging example

--- tests/diff_sim.py
+++ tests/diff_sim.py
@@ -1,67 +1,53 @@
-import os
-import sys
+'''
+Example to show why averaging the accumulated loss must pre-compute the
+coeficient of the batch size, instead of adding followed by division.
+
+Given:
+x : array [0, 1, 2]
+if mean is True
+    loss = sum(x)
+else
+    loss = sum(x) / size(x)
+
+Goal:
+Compute the gradient d(loss) / d(x)
+'''
 import numpy as np
 import taichi as ti
-from taichi_pushing.physics.composite_util import Composite2D
-from taichi_pushing.physics.pushing_simulator import PushingSimulator
-from taichi_pushing.physics.utils import Defaults
 
-np.random.seed(0)
-
-DTYPE = Defaults.DTYPE
-
-ti.init(arch=ti.cpu, debug=False)
+ti.init(arch=ti.cpu, debug=True)
 
 @ti.data_oriented
-class Loss():
-    """docstring for Loss"""
-    def __init__(self, engines):
-        self.loss = ti.field(DTYPE, shape=(), needs_grad=True)
-        self.engines = engines
+class TaichiTest:
+    def __init__(self) -> None:
+        self.n = 3
+        self.x = ti.field(ti.f64, self.n, needs_grad=True)
+        self.a = ti.field(ti.f64, shape=())
+        self.loss = ti.field(ti.f64, shape=(), needs_grad=True)
+        
+    @ti.kernel
+    def add_loss(self, i: ti.i32):
+        self.loss[None] += self.a[None] * self.x[i]
 
     @ti.kernel
-    def clear_loss(self):
-        self.loss[None] = 0
+    def average_loss(self, b: ti.f64):
+        self.loss[None] /= b
 
-    @ti.kernel
-    def compute_loss(self, t: ti.i32):
-        for i in sim_est.composite_geom_id:
-            self.loss[None] += (self.engines[0].geom_pos[t, i][0] - self.engines[1].geom_pos[t, i][0])**2 + \
-                    (self.engines[0].geom_pos[t, i][1] - self.engines[1].geom_pos[t, i][1])**2
+    def comppute_loss(self, mean=True):
+        for i in range(self.n):
+            self.add_loss(i)
+        if mean:
+            self.average_loss(self.n)
 
-obj_idx = 0
-composite_est, composite_gt = Composite2D(obj_idx), Composite2D(obj_idx)
-sim_est, sim_gt = PushingSimulator(composite_est), PushingSimulator(composite_gt)
+if __name__ == '__main__':
+    taichi_test = TaichiTest()
+    taichi_test.x.from_numpy(np.arange(taichi_test.n))
+    taichi_test.a[None] = 1
 
-sim_gt.composite_mass.from_numpy(composite_est.mass_dist)
-sim_est.composite_mass.from_numpy(np.random.rand(*composite_est.mass_dist.shape))
-sim_gt.clear_all()
-sim_est.clear_all()
+    with ti.ad.Tape(taichi_test.loss, validation=True):
+        taichi_test.comppute_loss(mean=True)
+    print("When mean is True, Loss: %.5f  Gradient: "%(taichi_test.loss[None]), taichi_test.x.grad)
 
-num_particle = composite_est.num_particle
-
-loss = Loss((sim_est, sim_gt))
-
-def run_world(sim, action_idx):
-    sim.initialize(action_idx)
-    for s in range(sim.max_step-1):
-        sim.collide(s)
-        sim.compute_ft(s)
-        sim.update(s)
-        # sim.render(s)
-
-def run_episode(action_idx=10):
-    run_world(sim_est, action_idx)
-    run_world(sim_gt, action_idx)
-
-def forward():
-    run_episode()
-    loss.compute_loss(100)
-
-with ti.Tape(loss.loss):
-    forward()
-
-# forward()
-
-print(loss.loss[None])
-print(sim_est.composite_mass.grad)
+    with ti.ad.Tape(taichi_test.loss, validation=True):
+        taichi_test.comppute_loss(mean=False)
+    print("When mean is False, Loss: %.5f  Gradient: "%(taichi_test.loss[None]), taichi_test.x.grad)
